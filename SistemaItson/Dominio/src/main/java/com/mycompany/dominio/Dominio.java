261fb4cc80c869a410ccbfda66bbb379d744d6da
Adel Mendez <adelmendez2@gmail.com>
2025-06-21 19:16:04 -0700

    correcion del pom

--- SistemaItson/Dominio/src/main/java/com/mycompany/dominio/Dominio.java
+++ SistemaItson/Dominio/src/main/java/com/mycompany/dominio/Dominio.java
@@ -1,8 +1,10 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.dominio;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
@@ -11,6 +13,7 @@ package com.mycompany.dominio;
 public class Dominio {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("conexionJPA");
+
     }
 }
